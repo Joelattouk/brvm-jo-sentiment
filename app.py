1601ceee3f9c6b382e8f5781a818e5a322fe8ed1
Joelattouk <82134532+Joelattouk@users.noreply.github.com>
2023-05-24 10:39:27 +0000

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import dash
+import scipy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
